b714241b62f295200cca4b6d0b4fef08f1c32fff
Azhuo <2323458642@qq.com>
2015-09-28 17:08:21 +0800

    ApiTask

--- app/src/com/azhuoinfo/pshare/fragment/HomeFragment.java
+++ app/src/com/azhuoinfo/pshare/fragment/HomeFragment.java
@@ -12,18 +12,25 @@ import android.widget.LinearLayout;
 
 import com.azhuoinfo.pshare.AccountVerify;
 import com.azhuoinfo.pshare.R;
-import com.azhuoinfo.pshare.api.ApiContants;
-import com.azhuoinfo.pshare.api.task.ApiTask;
-import com.azhuoinfo.pshare.api.task.OnDataLoader;
-import com.azhuoinfo.pshare.model.UserAuth;
+import com.azhuoinfo.pshare.view.ListViewUtils;
 
+import mobi.cangol.mobile.actionbar.ActionMenu;
+import mobi.cangol.mobile.actionbar.ActionMenuItem;
 import mobi.cangol.mobile.base.BaseContentFragment;
 import mobi.cangol.mobile.base.FragmentInfo;
 
 public class HomeFragment extends BaseContentFragment {
+	private Button mMineHomeButton;
+	private boolean isFrist=true;
+	private int mOnClick=0;
+	private LinearLayout mMineHomeButtonLinearLayout;
+	private Button mChangeButton;
 	private AccountVerify mAccountVerify;
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		//this.getCustomActionBar().setDisplayShowHomeEnabled(false);
+		this.getCustomActionBar().setCustomHomeAsUpIndicator(R.drawable.homepager_user, R.drawable.homepager_user);
+
 		mAccountVerify = AccountVerify.getInstance(getActivity());
 	}
 
@@ -49,18 +56,73 @@ public class HomeFragment extends BaseContentFragment {
 
 	@Override
 	protected void findViews(View view) {
+		mMineHomeButton=(Button) view.findViewById(R.id.button_mine_home);
+		mChangeButton=(Button) view.findViewById(R.id.button_change);
+		mMineHomeButtonLinearLayout=(LinearLayout) view.findViewById(R.id.ll_mine_home);
 	}
-	
+
 	@Override
 	protected void initViews(Bundle savedInstanceState) {
 		this.setTitle(R.string.title_home);
-		
+		mChangeButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				replaceFragment(MineHomeFragment.class,"MineHomeFragment",null);
+			}
+		});
+		mMineHomeButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				mOnClick++;
+				if(mOnClick%2==1){
+					AnimationSet set = new AnimationSet(true);
+					TranslateAnimation translate = new TranslateAnimation(Animation.RELATIVE_TO_PARENT,0,Animation.RELATIVE_TO_PARENT,0,
+							Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0);
+					set.addAnimation(translate);
+					set.setDuration(500);
+					set.setFillAfter(true);
+					mMineHomeButtonLinearLayout.offsetTopAndBottom(-mMineHomeButtonLinearLayout.getHeight());
+					mMineHomeButtonLinearLayout.startAnimation(set);
+				}
+				else{
+					AnimationSet set = new AnimationSet(true);
+					TranslateAnimation translate = new TranslateAnimation(Animation.RELATIVE_TO_PARENT,0,Animation.RELATIVE_TO_PARENT,0,
+							Animation.RELATIVE_TO_SELF, -0.5f, Animation.RELATIVE_TO_SELF, 0);
+					set.addAnimation(translate);
+					set.setDuration(500);
+					set.setFillAfter(true);
+					mMineHomeButtonLinearLayout.offsetTopAndBottom(mMineHomeButtonLinearLayout.getHeight());
+					mMineHomeButtonLinearLayout.startAnimation(set);
+				}
+			}
+		});
+
 	}
 
 	@Override
 	protected void initData(Bundle savedInstanceState) {
-		
+
 	}
+	@Override
+	protected boolean onMenuActionCreated(ActionMenu actionMenu) {
+		super.onMenuActionCreated(actionMenu);
+		actionMenu.add(new ActionMenuItem(1, "搜索", R.drawable.search1, 1));
+		actionMenu.add(new ActionMenuItem(2,"代伯区详情",R.drawable.list_car,2));
+		return true;
+	}
+	@Override
+	public boolean onMenuActionSelected(ActionMenuItem action) {
+		switch(action.getId()){
+			case 1:
+				this.getCustomActionBar().startSearchMode();
+				break;
+			case 2:
+				replaceFragment(ParkingDetailsFragment.class,"ParkingDetailsFragment",null);
+				break;
+		}
+		return super.onMenuActionSelected(action);
+	}
+
 
 	@Override
 	public void onSaveInstanceState(Bundle outState) {
@@ -71,7 +133,6 @@ public class HomeFragment extends BaseContentFragment {
 	protected FragmentInfo getNavigtionUpToFragment() {
 		return null;
 	}
-	
 
 
 	@Override
---
+++ app/src/com/azhuoinfo/pshare/fragment/MonthlyRentCarfinishPayFragment.java
@@ -0,0 +1,119 @@
+package com.azhuoinfo.pshare.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.azhuoinfo.pshare.AccountVerify;
+import com.azhuoinfo.pshare.R;
+
+import mobi.cangol.mobile.base.BaseContentFragment;
+import mobi.cangol.mobile.base.FragmentInfo;
+
+/**
+ * Created by Azhuo on 2015/9/22.
+ */
+public class MonthlyRentCarFinishPayFragment extends BaseContentFragment{
+    //返回上一个页面
+    //private Button mBackButton;
+
+    //国
+    private TextView mCountryTextView;
+    //省
+    private TextView mProvinceTextView;
+    //市
+    private TextView mCityTextView;
+    //区
+    private TextView mAreaTextView;
+    //设置小区
+    private TextView mLivingAreaTextView;
+    //时间段
+    private TextView mTimeFrameTextView;
+    //开始时间
+    private TextView mMonthlyrentBegintTimeTextView;
+    //结束时间
+    private TextView mMonthlyrentEndTimeTextView;
+    //单价
+    private TextView mMonthlyrentPriceTextView;
+    //姓名
+    private EditText mCustomerNameEditText;
+    //车牌
+    private TextView mCarNumberTextView;
+
+    private AccountVerify mAccountVerify;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mAccountVerify = AccountVerify.getInstance(getActivity());
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+
+        return inflater.inflate(R.layout.fragment_monthlyrent_car_finish_pay,container,false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        findViews(view);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        initViews(savedInstanceState);
+        initData(savedInstanceState);
+    }
+    @Override
+    protected void findViews(View view) {
+        //mBackButton=(Button) view.findViewById(R.id.activity_back);
+
+        mCountryTextView=(TextView) view.findViewById(R.id.tv_country);
+        mProvinceTextView=(TextView) view.findViewById(R.id.tv_province);
+        mCityTextView=(TextView) view.findViewById(R.id.tv_city);
+
+        mAreaTextView=(TextView) view.findViewById(R.id.tv_area);
+
+        mLivingAreaTextView=(TextView) view.findViewById(R.id.tv_living_area);
+
+        mTimeFrameTextView=(TextView) view.findViewById(R.id.tv_time_frame);
+
+        mMonthlyrentBegintTimeTextView=(TextView) view.findViewById(R.id.tv_monthlyrent_begint_time);
+
+        mMonthlyrentEndTimeTextView=(TextView) view.findViewById(R.id.tv_monthlyrent_end_ime);
+
+        mMonthlyrentPriceTextView=(TextView) findViewById(R.id.tv_monthlyrent_price);
+
+        mCustomerNameEditText=(EditText) view.findViewById(R.id.et_customer_name);
+
+        mCarNumberTextView=(TextView) view.findViewById(R.id.car_number);
+    }
+
+    @Override
+    protected void initViews(Bundle bundle) {
+       this.setTitle(R.string.rent);
+
+    }
+
+    @Override
+    protected void initData(Bundle bundle) {
+
+    }
+
+    @Override
+    protected FragmentInfo getNavigtionUpToFragment() {
+        return null;
+    }
+
+    @Override
+    public boolean isCleanStack() {
+        return false;
+    }
+}
