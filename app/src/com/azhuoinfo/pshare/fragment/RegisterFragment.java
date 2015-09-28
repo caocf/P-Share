6028db912714a2456016513d18ddb3dc4c63789f
Azhuo <2323458642@qq.com>
2015-09-28 17:28:32 +0800

    ApiTask

--- app/src/com/azhuoinfo/pshare/fragment/RegisterFragment.java
+++ app/src/com/azhuoinfo/pshare/fragment/RegisterFragment.java
@@ -12,8 +12,14 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.azhuoinfo.pshare.R;
+import com.azhuoinfo.pshare.api.ApiContants;
+import com.azhuoinfo.pshare.api.task.ApiTask;
+import com.azhuoinfo.pshare.api.task.OnDataLoader;
+import com.azhuoinfo.pshare.model.UserAuth;
+
 import mobi.cangol.mobile.base.BaseContentFragment;
 import mobi.cangol.mobile.base.FragmentInfo;
+import mobi.cangol.mobile.logging.Log;
 
 
 public class RegisterFragment extends BaseContentFragment {
@@ -92,6 +98,36 @@ public class RegisterFragment extends BaseContentFragment {
 	public boolean isCleanStack() {
 		return false;
 	}
+	private void postRegister(String mobile,String password) {
+		ApiTask apiTask=ApiTask.build(this.getActivity(),TAG);
+		apiTask.setUrl(ApiContants.instance(getActivity()).getActionUrl(ApiContants.API_CUSTOMER_REGISTER));
+		apiTask.setParams(ApiContants.instance(getActivity()).register(mobile, password));
+		apiTask.setRoot(null);
+		apiTask.execute(new OnDataLoader<UserAuth>() {
 
+			@Override
+			public void onStart() {
+				if (getActivity() != null){
 
+				}
+			}
+
+			@Override
+			public void onSuccess(boolean page, UserAuth auth) {
+				if (getActivity() != null) {
+
+				}
+			}
+
+			@Override
+			public void onFailure(String code, String message) {
+				Log.d(TAG, "code=:" + code + ",message=" + message);
+				if (getActivity() != null) {
+
+				}
+
+			}
+
+		});
+	}
 }
