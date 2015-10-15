e515ea8f0cde49fc91ee6fffb3c43f7e39f14260
Azhuo <2323458642@qq.com>
2015-10-15 09:26:10 +0800

    预约订单

--- app/src/com/azhuoinfo/pshare/fragment/ParkingDetailsFragment.java
+++ app/src/com/azhuoinfo/pshare/fragment/ParkingDetailsFragment.java
@@ -2,6 +2,7 @@ package com.azhuoinfo.pshare.fragment;
 
 import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
